983463fef7fc9e9954ef19a6fddf9ac41dadd543
Josh Morris <jmo0624@gmail.com>
2020-01-24 00:19:08 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
-
-  create_table "artists", force: :cascade do |t|
-    t.string "name"
-  end
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "songs", force: :cascade do |t|
     t.string "name"
